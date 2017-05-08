cea4e521399ac510b39c267827e0ec0246066ebb
Luciano Porto <luciano.bapo@gmail.com>
2017-05-08 11:19:59 -0300

    automatic-commit-alias

---
+++ resources/lang/pt/general.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: luciano
+ * Date: 08/05/17
+ * Time: 11:18
+ */
+
+return [
+
+    'failed' => 'These credentials do not match our records.',
+    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+
+];
