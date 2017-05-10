353701bdbefc75388665be71b7410deadfacfdf4
Luciano Porto <luciano.bapo@gmail.com>
2017-05-10 11:32:14 -0300

    automatic-commit-alias

--- resources/views/data/tabs/employee.blade.php
+++ resources/views/data/tabs/employee.blade.php
@@ -33,25 +33,25 @@
 
                 </div>
 
-                {{--<nav>--}}
-                    {{--<ul class="pagination">--}}
-                        {{--<li v-if="pagination.current_page > 1">--}}
-                            {{--<a href="#" aria-label="Previous" @click.prevent="changePage(pagination.current_page - 1)">--}}
-                                {{--<span aria-hidden="true">«</span>--}}
-                            {{--</a>--}}
-                        {{--</li>--}}
-                        {{--<li v-for="page in pagesNumber" v-bind:class="[ page == isActived ? 'active' : '']">--}}
-                            {{--<a href="#" @click.prevent="changePage(page)">--}}
-                                {{--@{{ page }}--}}
-                            {{--</a>--}}
-                        {{--</li>--}}
-                        {{--<li v-if="pagination.current_page < pagination.last_page">--}}
-                            {{--<a href="#" aria-label="Next" @click.prevent="changePage(pagination.current_page + 1)">--}}
-                                {{--<span aria-hidden="true">»</span>--}}
-                            {{--</a>--}}
-                        {{--</li>--}}
-                    {{--</ul>--}}
-                {{--</nav>--}}
+                <nav v-if="pagination.total>0">
+                    <ul class="pagination">
+                        <li v-if="pagination.current_page > 1">
+                            <a href="#" aria-label="Previous" @click.prevent="changePage(pagination.current_page - 1)">
+                                <span aria-hidden="true">«</span>
+                            </a>
+                        </li>
+                        <li v-for="page in pagesNumber" v-bind:class="[ page == isActived ? 'active' : '']">
+                            <a href="#" @click.prevent="changePage(page)">
+                                @{{ page }}
+                            </a>
+                        </li>
+                        <li v-if="pagination.current_page < pagination.last_page">
+                            <a href="#" aria-label="Next" @click.prevent="changePage(pagination.current_page + 1)">
+                                <span aria-hidden="true">»</span>
+                            </a>
+                        </li>
+                    </ul>
+                </nav>
 
                 @include('erpnetSaas::data.tabs.employee.create')
                 @include('erpnetSaas::data.tabs.employee.edit')
