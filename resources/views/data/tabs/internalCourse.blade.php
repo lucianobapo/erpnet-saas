f18cfc33ab6b2801be8fc900c4a6de3bc1347794
Luciano Porto <luciano.bapo@gmail.com>
2017-05-13 13:20:32 -0300

    automatic-commit-alias

--- resources/views/data/tabs/internalCourse.blade.php
+++ resources/views/data/tabs/internalCourse.blade.php
@@ -13,7 +13,7 @@
                     </div>
                 </div>
 
-                <div class="row" v-if="pagination.total==0">
+                <div class="row" v-if="computedItems.length==0">
                     <div class="col-xs-12 well well-sm text-center">
                         <em>{{ t('No records found') }}</em>
                     </div>
@@ -21,8 +21,8 @@
 
                 @include('erpnetSaas::data.tabs.parts.nav')
 
-                <div class="row" v-if="pagination.total>0">
-                    <div v-for="item in items" class="col-xs-12 well well-sm">
+                <div class="row" v-if="computedItems.length>0">
+                    <div v-for="item in computedItems" class="col-xs-12 well well-sm">
                         <div class="row">
                             <div class="col-xs-12 col-sm-8 col-md-6">
                                 <ul>
