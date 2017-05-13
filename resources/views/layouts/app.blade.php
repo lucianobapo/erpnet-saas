d19764ef2c4e2c7f93ea7a47e7da7ff86e081b01
Luciano Porto <luciano.bapo@gmail.com>
2017-05-13 18:26:05 -0300

    automatic-commit-alias

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -28,6 +28,16 @@
     <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
     <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
     <![endif]-->
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Scripts -->
+    <script type="text/javascript">
+        window.Laravel = '{!! json_encode([
+            'csrfToken' => csrf_token(),
+        ]) !!}';
+    </script>
 </head>
 <body id="spark-layout">
 <!-- Vue App For Spark Screens -->
